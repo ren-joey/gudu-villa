5899dc86c18e0ebd2e06f67e17873bc90149491e
TsuYi Ren <tsuyiren@gmail.com>
2018-09-04 01:32:46 +0800

    20180904 windows update

---
+++ dist/action/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CyberShake</title>
+
+	<link href="../css/mainCSS.css" rel="stylesheet" type="text/css">
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <style>
+    body{
+    margin:0;
+    padding:0;
+    }
+    </style>
+
+    <?php
+
+    // $str = '商店名稱：'.$_GET['inputName'] .'。電子信箱：'. $_GET['inputEmail'] .'。連絡電話：'. $_GET['inputPhone'];
+    $str = '測試';
+
+    require 'PHPMailerAutoload.php'; // path to the PHPMailer class
+
+    $mail = new PHPMailer;
+
+    //Tell PHPMailer to use SMTP
+    $mail->isSMTP();
+
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+
+    //Ask for HTML-friendly debug output
+    $mail->Debugoutput = 'html';
+
+    $mail->CharSet = "utf-8";
+
+    //Set the hostname of the mail server
+    $mail->Host = 'smtp.mail.yahoo.com';
+
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 465;
+
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "ch1540@yahoo.com.tw";
+
+    //Password to use for SMTP authentication
+    $mail->Password = "ch063699";
+
+    //Set who the message is to be sent from
+    $mail->setFrom('ch1540@yahoo.com.tw', '湧動創意');
+
+    //Set an alternative reply-to address
+    $mail->addReplyTo('tsuyiren@gmail.com', '湧動創意');
+
+    //Set who the message is to be sent to
+    $mail->addAddress('tsuyiren@gmail.com', '湧動創意');
+
+    //Set the subject line
+    $mail->Subject = '您有一個新房客';
+
+    //Replace the plain text body with one created manually
+    $mail->Body = $str;
+    ?>
+</head>
+<body>
+
+<div class="join-page-php">
+<h2 class="form-signin-heading" style="margin:0 !important; padding:0 !important;">
+<?php
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "提交失敗，請連繫管理人員。";
+} else {
+    echo "提交完成";
+}
+
+?>
+</h2>
+</div>
+</body>
+</html>
